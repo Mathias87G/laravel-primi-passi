d0a71f48f4b27f6f8b7b0b0f0e7a8c8b1ec75fee
Mathias <mathias.grandin@gmail.com>
2020-10-08 16:21:52 +0200

    prodotti ok

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+  <img id="Logo" src="https://cdn.shortpixel.ai/client/q_lossy,ret_img,w_250,h_145/https://www.lamolisana.it/wp-content/uploads/2020/05/marchio-sito-test.png" alt="">
+  <nav>
+    <ul class="menu">
+      <li><a href="{{route('home')}}">Home</a></li>
+      <li><a href="{{route('prodotti')}}">Prodotti</a></li>
+      <li><a href="{{route('contatti')}}">Contatti</a></li>
+    </ul>
+  </nav>
+</header>
